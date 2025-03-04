3d7d8ce0021a455982886d9f464071a0bb69024a
Exaynts <exayntszhenya@gmail.com>
2025-03-04 21:34:47 +0300

    Delete semester 2/Work#2 directory

--- semester 2/Work#2/Task 2/Task 2 (With several files)/RectangleDrawing — копия.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Rectangle.h"
-using namespace std;
-//Отрисовка прямоугольника с помощью псевдографики
-void RectangleDrawing(float x1, float x2, float y1, float y2) {
-    cout << "*Getting rectangle's image and its diagonal*" << endl;
-
-}
