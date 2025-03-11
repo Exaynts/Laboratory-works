264d2ba548aa3ec72300df9f0944a64b46442576
Exaynts <exayntszhenya@gmail.com>
2025-03-11 15:14:34 +0300

    Update SimpleActionsWithRectangle.cpp

--- semester 2/Work #2/Task 2/Task #2 in several files/SimpleActionsWithRectangle.cpp
+++ semester 2/Work #2/Task 2/Task #2 in several files/SimpleActionsWithRectangle.cpp
@@ -11,20 +11,20 @@ using namespace std;
         y2 = 0;
     }
     //Конструктор перезагрузки с параметрами;
-    Rectangle(float p_x1, float p_x2, float p_y1, float p_y2) {
+    Rectangle(int p_x1, int p_x2, int p_y1, int p_y2) {
         x1 = p_x1;
         y1 = p_y1;
         x2 = p_x2;
         y2 = p_y2;
     }
     // Обработка значений полей (Вычисление длины диагонали)
-    float CalculatingOfDiagonal(float x1, float x2, float y1, float y2) {
+    float CalculatingOfDiagonal(int x1, int x2, int y1, int y2) {
         float diagonal;
         diagonal = pow(((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)), 0.5);
         return diagonal;
     }
     //Строка информации об объекте
-    void InfoOfRectangle(float x1, float x2, float y1, float y2) {
+    void InfoOfRectangle(int x1, int x2, int y1, int y2) {
         cout << endl << "x1 = " << x1 << ", x2 = " << x2 << ", y1 = " << y1 << ", y2 = " << y2 <<
             ", rectangle's diagonal = " << CalculatingOfDiagonal(x1, y1, x2, y2) << endl;
     }
