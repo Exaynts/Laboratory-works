e41b0df11e1f58c796a433cc5d2b9cd7ab85925b
Exaynts <exayntszhenya@gmail.com>
2025-04-03 22:48:34 +0300

    Create main.cpp

---
+++ semester 2/Work#5 /Task #2 /main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include "Numbers.h"
+using namespace std;
+
+int main() {
+    // Создание массива чисел
+    Numbers();
+    // Ввод данных
+
+    // Вывод данных
+    return 0;
+}
