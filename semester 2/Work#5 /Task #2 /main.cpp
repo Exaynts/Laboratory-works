1e1407c8bbf0415a114bf14944cc93799df969a2
Exaynts <exayntszhenya@gmail.com>
2025-04-04 22:19:34 +0300

    Update main.cpp

--- semester 2/Work#5 /Task #2 /main.cpp
+++ semester 2/Work#5 /Task #2 /main.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
-#include <vector>
-#include "Numbers.h"
 using namespace std;
 
 int main() {
-    // Создание массива чисел
-    Numbers();
     // Определение массивов
-    int n1, n2;
+    /*
+    int n1, n2; // Длины массивов
     cout << "n1 = ";
     cin >> n1;
     cout << "n2 = ";
     cin >> n2;
-    numbers1 = Numbers(n1);
-    numbers2 = Numbers(n2);
+    */
+    int n1 = 3;
+    int n2 = 2;
+    //Numbers() numbers1;
+    Numbers numbers1(n1);
+    Numbers numbers2(n2);
+
+    numbers1.Input(numbers1);
+
     // Вывод данных
     return 0;
 }
