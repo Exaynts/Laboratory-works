159c746b91f7bcfb826db956f74befbcf58d10a2
Exaynts <exayntszhenya@gmail.com>
2025-04-04 22:22:25 +0300

    Update Numbers.h

--- semester 2/Work#5 /Task #2 /Numbers.h
+++ semester 2/Work#5 /Task #2 /Numbers.h
@@ -1,22 +1,37 @@
 #pragma once
 #include <iostream>
+#include <vector>
 #include "Numbers.cpp"
 using namespace std;
 
 class Numbers {
+    int lenght; // Длина массива чисел
+    vector <int> nums;
 public:
     // Конструктор класса массива чисел
-    Numbers() {}
-    // Длина массива чисел
-    int Lenght(vector <int> numbers) {}
-    // Обращение к элементу массива
-    int Index(vector <int> numbers, int i) {}
+    Numbers() {
+    }
+    // Конструктор класса массива чисел произвольной длины
+    Numbers(int len) {
+    }
+    int Get_lenght() {
+    }
+    
     // Заполнение массива
-    vector <int> Input(vector <int> numbers) {}
+    Numbers Input(Numbers& numbers) {
+    }
+
     // Сложение/Вычитание массивов
-    vector <int > Sum(vector <int> numbers1, vector <int> numbers2, char sign) {}
-    // Деструктор класса массива чисел
-    ~Numbers() {
-        cout << "Numbers-object has been destructed" << endl;
+    Numbers Sum(Numbers numbers1, Numbers numbers2, char sign) { // sign это "+"" или "-"
+    }
+    // Умножение/деление массива на скаляр
+    Numbers Multiply(Numbers numbers, int scalar, char sign) { // sign это "*" или "/"
+    }
+
+    // Вывод элемента массива по индексу
+    void print(Numbers numbers, int i) {
+    }
+    // Вывод всего массива
+    void print_all(Numbers numbers) {
     }
 } numbers;
