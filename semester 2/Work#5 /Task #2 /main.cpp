7c0d4444d413d00bee54c7b89c0c60b3f02b4c50
Exaynts <exayntszhenya@gmail.com>
2025-04-04 20:35:23 +0300

    Update main.cpp

--- semester 2/Work#5 /Task #2 /main.cpp
+++ semester 2/Work#5 /Task #2 /main.cpp
@@ -6,8 +6,14 @@ using namespace std;
 int main() {
     // Создание массива чисел
     Numbers();
-    // Ввод данных
-
+    // Определение массивов
+    int n1, n2;
+    cout << "n1 = ";
+    cin >> n1;
+    cout << "n2 = ";
+    cin >> n2;
+    numbers1 = Numbers(n1);
+    numbers2 = Numbers(n2);
     // Вывод данных
     return 0;
 }
