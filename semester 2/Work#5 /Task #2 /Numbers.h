8f2cb31c41a769ec0610559af709d244686357e4
Exaynts <exayntszhenya@gmail.com>
2025-04-04 20:35:39 +0300

    Update Numbers.h

--- semester 2/Work#5 /Task #2 /Numbers.h
+++ semester 2/Work#5 /Task #2 /Numbers.h
@@ -6,11 +6,15 @@ using namespace std;
 class Numbers {
 public:
     // Конструктор класса массива чисел
-    Numbers();
+    Numbers() {}
     // Длина массива чисел
     int Lenght(vector <int> numbers) {}
     // Обращение к элементу массива
     int Index(vector <int> numbers, int i) {}
+    // Заполнение массива
+    vector <int> Input(vector <int> numbers) {}
+    // Сложение/Вычитание массивов
+    vector <int > Sum(vector <int> numbers1, vector <int> numbers2, char sign) {}
     // Деструктор класса массива чисел
     ~Numbers() {
         cout << "Numbers-object has been destructed" << endl;
