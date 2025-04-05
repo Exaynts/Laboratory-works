7560af54c4ef1636141b48677b42558fb8951031
Exaynts <exayntszhenya@gmail.com>
2025-04-05 08:40:20 +0300

    Update Numbers.cpp

--- semester 2/Work#5 /Task #2 /Numbers.cpp
+++ semester 2/Work#5 /Task #2 /Numbers.cpp
@@ -3,23 +3,27 @@
 using namespace std;
 
 
+class Numbers {
+    int length; // Длина массива чисел
+    vector <int> nums;
+public:
     // Конструктор класса массива чисел
-    Numbers() {
-        int lenght = 1;
-        vector <int> numbers(lenght);
+    Numbers Numbers() {
+        int length = 1;
+        vector <int> numbers(length);
     }
     // Конструктор класса массива чисел произвольной длины
-    Numbers(int len) {
-        int lenght = len;
-        vector <int> numbers(lenght);
+    Numbers Numbers(int len) {
+        int length = len;
+        vector <int> numbers(length);
     }
-    int Get_lenght() {
-        return lenght;
+    int Get_length() {
+        return numbers.length;
     }
     
     // Заполнение массива
-    Numbers Input(Numbers& numbers) {
-        for (int i = 0; i < lenght; i++) {
+    Numbers Input(Numbers* numbers2) {
+        for (int i = 0; i < numbers.Get_length(); i++) {
             int value;
             cout << endl << "i = ";
             cin >> value;
@@ -30,7 +34,7 @@ using namespace std;
 
     // Сложение/Вычитание массивов
     Numbers Sum(Numbers numbers1, Numbers numbers2, char sign) { // sign это "+"" или "-"
-        if (numbers1.Get_lenght() != numbers2.Get_lenght()) {
+        if (numbers1.Get_length() != numbers2.Get_length()) {
             cout << "Длины массивов различны!" << endl;
             return numbers1;
         }
@@ -44,8 +48,8 @@ using namespace std;
                 cout << "Char error!";
                 return numbers1;
             }
-            Numbers Sum(numbers1.Get_lenght());
-            for (int i = 0; i < numbers1.Get_lenght(); i++) {
+            Numbers Sum(numbers1.Get_length());
+            for (int i = 0; i < numbers1.Get_length(); i++) {
                 Sum.nums[i] = numbers1.nums[i] + s*numbers2.nums[i];
             }
             return Sum;
@@ -54,7 +58,7 @@ using namespace std;
     // Умножение/деление массива на скаляр
     Numbers Multiply(Numbers numbers, int scalar, char sign) { // sign это "*" или "/"
         if (sign == '*') {
-            for (int i = 0; i < numbers.Get_lenght(); i++) {
+            for (int i = 0; i < numbers.Get_length(); i++) {
                 numbers.nums[i] *= scalar;
             }
         }
@@ -63,7 +67,7 @@ using namespace std;
                 cout << "Division by zero!" << endl;
             }
             else {
-                for (int i = 0; i < numbers.Get_lenght(); i++) {
+                for (int i = 0; i < numbers.Get_length(); i++) {
                     numbers.nums[i] /= scalar;
                 }
             }
@@ -76,7 +80,7 @@ using namespace std;
 
     // Вывод элемента массива по индексу
     void print(Numbers numbers, int i) {
-        if (i < numbers.Get_lenght()) {
+        if (i < numbers.Get_length()) {
             cout << numbers.nums[i] << endl;
         }
         else {
@@ -85,7 +89,8 @@ using namespace std;
     }
     // Вывод всего массива
     void print_all(Numbers numbers) {
-        for (int i = 0; i < numbers.Get_lenght(); i++) {
+        for (int i = 0; i < numbers.Get_length(); i++) {
             cout << numbers.nums[i] << endl;
         }
     }
+} numbers;
